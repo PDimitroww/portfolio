d3e3d0497b5d775ff7beb49e585652101457fa02
PDimitroww <plamen.vn.96@gmail.com>
2023-02-28 16:56:19 +0200

    Added a visitors counter

--- script.js
+++ script.js
@@ -497,3 +497,26 @@ function toggleTopButton() {
 }
 
 btnToTop.addEventListener('click', scrollToTop);
+
+//=================================== VISITORS COUNTER FUNCTIONALITY ===============================//
+
+const counterContainer = document.querySelector('.website-counter');
+// let resetButton = document.querySelector("#reset");
+let visitCount = localStorage.getItem('page_view');
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = +visitCount + 1;
+  localStorage.setItem('page_view', visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem('page_view', 1);
+}
+counterContainer.innerHTML = visitCount;
+
+// Adding onClick event listener
+// resetButton.addEventListener("click", () => {
+//   visitCount = 1;
+//   localStorage.setItem("page_view", 1);
+//   counterContainer.innerHTML = visitCount;
+// });
