b42cc0c1427b19df4f4582321e99c48de95ce028
PDimitroww <plamen.vn.96@gmail.com>
2023-04-12 14:03:41 +0300

    Optimized the accessibility

--- script.js
+++ script.js
@@ -294,7 +294,7 @@ const slider = function () {
     slides.forEach(function (_, i) {
       dotContainer.insertAdjacentHTML(
         'beforeend',
-        `<button class="dots__dot" data-slide="${i}"></button>`
+        `<button class="dots__dot" data-slide="${i}" aria-label="dots to navigate the slider "></button>`
       );
     });
   };
